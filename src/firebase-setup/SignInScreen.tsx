c4325c855ac998b3876ca6fb3029f2f1bdcb24c8
August Miller <augustamiller77@gmail.com>
2023-01-08 15:11:38 -0600

    Added guest login support

--- src/firebase-setup/SignInScreen.tsx
+++ src/firebase-setup/SignInScreen.tsx
@@ -1,19 +1,82 @@
 // Import FirebaseAuth and firebase.
-import React, { memo, useMemo } from "react";
+import React, { memo, useState } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import { uiConfig } from "./firebase-config";
 import { auth } from "./firebase-config";
 import { StyledFirebaseAuth } from "./StyledFirebaseAuth";
 import strings from "../app/strings";
 import { About } from "../features/about/About";
+import Spinner from "react-bootstrap/Spinner";
 
 export const SignInScreen = () => {
-  //override the default with whatever we provide tot he component
+  const [signInStatus, setStatus] = useState<"loading" | "error" | "done">(
+    "done"
+  );
+
+  const handleGuestSignIn = async () => {
+    if (signInStatus === "loading") {
+      return;
+    }
+    setStatus("loading");
+    auth
+      .signInAnonymously()
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if (errorCode === "auth/operation-not-allowed") {
+          alert("You must enable Anonymous auth in the Firebase Console.");
+        } else {
+          console.error(error);
+        }
+        setStatus("error");
+      })
+      .finally(() => {
+        setStatus("done");
+      });
+  };
+
   return (
     <div className="container my-4">
-      <div className="my-4 card">
+      <div className="m-2 card">
         <h1 className="my-4">Sign in to {strings.appName}</h1>
-        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+
+        {signInStatus === "loading" ? (
+          <div className="col">
+            <Spinner />
+          </div>
+        ) : (
+          <>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+            <span> OR </span>
+            <div className="firebaseui-container firebaseui-page-provider-sign-in firebaseui-id-page-provider-sign-in firebaseui-use-spinner">
+              <div className="firebaseui-card-content">
+                <ul className="firebaseui-idp-list">
+                  <li className="firebaseui-list-item">
+                    <button
+                      className="firebaseui-idp-button mdl-button mdl-js-button mdl-button--raised"
+                      onClick={handleGuestSignIn}
+                      style={{ backgroundColor: "black" }}
+                    >
+                      <span className="firebaseui-idp-icon-wrapper text-light">
+                        <i className={"bi bi-person-x-fill"}></i>
+                      </span>
+
+                      <span className="firebaseui-idp-text firebaseui-idp-text-long">
+                        Continue as Guest
+                      </span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </>
+        )}
+
+        {signInStatus === "error" && (
+          <div>Something went wrong, please try again.</div>
+        )}
       </div>
       <div className="my-4"></div>
       <Accordion defaultActiveKey="0">
